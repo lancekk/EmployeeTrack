d2c021a4f01c7b38308b57c68928cf266551514a
Lance Kelley <lancekkcodes@gmail.com>
2021-10-31 20:47:34 -0500

    reset schema in seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,37 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
 USE employee_db;
+
+CREATE TABLE departments (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    dept_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10,2) NOT NULL,
+    department_id INTEGER,
+    FOREIGN KEY (department_id)
+    REFERENCES departments(id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employees (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER,
+    manager_id INTEGER DEFAULT (NULL),
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id)
+    ON DELETE SET NULL,
+    FOREIGN KEY (manager_id)
+    REFERENCES employees(id)
+    ON DELETE SET NULL
+);
+
 
 INSERT INTO departments (dept_name)
 VALUES ("Software"), ("Advertising");
